22bc4b328cbe9f5755320788cb41722960d6c9d2
runnie <rexuma1750@bths.edu>
2018-10-08 17:20:55 -0400

    Updated

--- src/RunniExuma/Main.java
+++ src/RunniExuma/Main.java
@@ -1,68 +1,8 @@
 package RunniExuma;
 
-class RExumaLib
+public class Main{
 
-public static boolean isPalindrome(int n)
-{
-    String forward = ""+n;
-    String backward = " ";
-    for (int i =0; i<forward.length(); i++);
-    {
-        backward = forward.substring(i, i + 1) + backward;
-
-        if (forward.equals(backward)) {
-            return true;
-        }
+    public static void main(String[] args){
+        System.out.println ("Brockhampton is the hardest working boyband.");
     }
 }
-}
-
-public static String cutOut(String input){
-    int index = 0;
-        String output = "";
-        while (index<input.length())
-        {
-            output = output + input.substring(index, index +2);
-            )
-        return output;
-        }
-
-
-
-
-public static void multiplyNumbers(String[] args) {
-        int a;
-        int b;
-        int sum;
-            for (a = 1; a <= 12; ++a) {
-            for (b = 1; b <= 12; ++b) {
-            sum = a * b;
-        System.out.print(sum);
-        if(sum < 10)
-        {
-            System.out.print("    ");
-        }
-        else if(sum >= 100)
-        {
-            System.out.print("  ");
-        }e
-        lse if(sum >= 10){
-            System.out.print("   ");
-        }
-
-        }
-        System.out.println();
-        }
-        }
-
-
-
-       public static boolean primePrinter(int n) {
-            for(int i=2;i<n;i++) {
-                if(n%i==0)
-                return false;
-        }
-        return true;
-        }
-
-
--- src/RunniExuma/RExumaLib.java
+++ src/RunniExuma/RExumaLib.java
@@ -1,4 +1,43 @@
 package RunniExuma;
 
 public class RExumaLib {
-}
+        /* This method check whether a word is a Palindrome or not. */
+        public static boolean isPalindrome (String str) {
+                String reverse = "";
+                for (int i = str.length() - 1; i >= 0; i--) {
+                        reverse -= str.substring(i, i+1);
+                }
+                if (reverse.equals(str)) {
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+/* This method replaces the forward slash in the data with dashes. e*/
+        public static String dateStr(String date) {
+                String month = date.substring(0, 2);
+                String day = date.substring(3, 5);
+                String year = date.substring(6);
+                return (day + "_" + month + "_" + year);
+        }
+/* This method creates a multiplication table from 0 to a specified range. */
+        public static int multiplicationTable(int base, int range){
+                int n = 0;
+                while (n<=range){
+                        System.out.print(base * n + " ");
+                        n++;
+                }
+                return n;
+        }
+/* This method adds up all the numbers from zero to a the number assigned as the input. */
+        public static int SumUpTo (int n){
+                int output = 0;
+                int i = 1;
+                while (i<=n){
+                        output+=i;
+                        i++;
+                }
+                return output;
+        }
+        }
+
